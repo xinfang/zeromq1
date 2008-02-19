820ba9386f3304fa8f5f5704b685edcb11f3b82f
Pavol Malosek <malosek@fastmq.com>
2008-02-19 11:40:16 +0000

    added ypipe latency test
    git-svn-id: http://svn.fastmq.org/svn/zeromq/trunk@100 7451a71c-b6c8-49cc-8254-141fc5361860

--- perf/tests/ysuite/ypipe_lat.cpp
+++ perf/tests/ysuite/ypipe_lat.cpp
@@ -20,10 +20,8 @@
 #include <unistd.h>
 
 #include "../../transports/ysuite.hpp"
-#include "../../helpers/time.hpp"
-#include "../../helpers/files.hpp"
-#include "../../workers/raw_receiver.hpp"
-#include "../../workers/raw_sender.hpp"
+#include "../../workers/echo.hpp"
+#include "../../workers/ping_pong.hpp"
 
 #include "test.hpp"
 
@@ -40,12 +38,13 @@ struct worker_args_t
 int main (void) {
 
     perf::ysuite_t transport (perf::active_sync_semaphore);
-    perf::raw_receiver_t receiver (TEST_MSG_COUNT_THRPUT);
+    perf::echo_t receiver (TEST_MSG_COUNT_LAT);
 
     worker_args_t w_args;
+
     w_args.id = 0;
     w_args.msg_size = 0;
-    w_args.msg_count = TEST_MSG_COUNT_THRPUT;
+    w_args.msg_count = TEST_MSG_COUNT_LAT;
     w_args.transport = transport.get_transport_1 ();
 
     pthread_t worker;
@@ -55,24 +54,10 @@ int main (void) {
         assert (rc == 0);
 
 
-    receiver.run (*transport.get_transport_2 (), "");
+    receiver.run (*transport.get_transport_2 (), "1_0_");
 
     rc = pthread_join (worker, NULL);
     assert (rc == 0);
-
-    perf::time_instant_t start_time;
-    perf::time_instant_t stop_time;
-
-    perf::read_times_1f (&start_time, &stop_time, "in.dat");
-
-    printf ("Test time: %llu [ms]\n", (stop_time - start_time) / 
-            (long long)1000);
-
-    // throughput [msgs/s]
-    unsigned long long msg_thput = ((long long) 1000000 * (long long) TEST_MSG_COUNT_THRPUT ) / 
-            (stop_time - start_time);
-
-    printf ("Your average throughput is %llu msgs/s\n", msg_thput);
 
     return 0;
 }
@@ -83,9 +68,9 @@ void *worker_function (void *args_)
     // args struct
     worker_args_t *w_args = (worker_args_t*)args_;
     
-    perf::raw_sender_t sender (w_args->msg_count, w_args->msg_size);
+    perf::ping_pong_t sender (w_args->msg_count, w_args->msg_size);
 
-    sender.run (*w_args->transport, "");
+    sender.run (*w_args->transport, "1_0_");
 
     return NULL;
 }
