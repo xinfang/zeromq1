c2d3274f73043be62af9b58ddf3a3d14dcd2751e
Martin Sustrik <sustrik@fastmq.com>
2008-04-21 13:16:59 +0000

    missing files added
    git-svn-id: http://svn.fastmq.org/svn/zeromq/branches/refactoring-for-0.3@165 7451a71c-b6c8-49cc-8254-141fc5361860

---
+++ zmq/pipe.hpp
@@ -0,0 +1,42 @@
+/*
+    Copyright (c) 2007-2008 FastMQ Inc.
+
+    This file is part of 0MQ.
+
+    0MQ is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    0MQ is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __ZMQ_PIPE_HPP_INCLUDED__
+#define __ZMQ_PIPE_HPP_INCLUDED__
+
+#include "ypipe.hpp"
+#include "atomic_counter.hpp"
+
+namespace zmq
+{
+
+    class pipe_t : public ypipe_t <void*, false>
+    {
+    public:
+
+        inline void revive ()
+        {
+        }
+
+    private:
+    }; 
+
+}
+
+#endif
