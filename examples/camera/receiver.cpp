96702b361ce0a873f8f0e58d395426017e9dbc59
Martin Lucina <mato@imatix.com>
2008-06-28 10:22:03 +0000

    Don't reinitialize SDL on each frame :-)
    git-svn-id: http://svn.fastmq.org/svn/zeromq/branches/refactoring-for-0.3@308 7451a71c-b6c8-49cc-8254-141fc5361860

--- examples/camera/receiver.cpp
+++ examples/camera/receiver.cpp
@@ -88,6 +88,8 @@ int main (int argc, char *argv [])
                SDL_Quit ();
                exit (1);
             }
+
+            sdl_initialised = true;
         }
 
         //  Create RGB surface
