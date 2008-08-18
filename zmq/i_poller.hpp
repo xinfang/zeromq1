040160de0d58095b0df4ad31000511bfc5282f29
Martin Sustrik <sustrik@fastmq.com>
2008-08-18 18:53:28 +0000

    i_poller documentation
    git-svn-id: http://svn.fastmq.org/svn/zeromq/trunk@408 7451a71c-b6c8-49cc-8254-141fc5361860

--- zmq/i_poller.hpp
+++ zmq/i_poller.hpp
@@ -30,12 +30,27 @@ namespace zmq
         //  such results in compiler warnings with some compilers.
         virtual ~i_poller () {};
 
+        //  Specify the file descriptor to use for polling.
         virtual void set_fd (int handle_, int fd_) = 0;
+
+        //  Start polling for input from socket.
         virtual void set_pollin (int handle_) = 0;
+
+        //  Stop polling for input from socket.
         virtual void reset_pollin (int handle_) = 0;
+
+        //  Try to read from socket (even though we are not sure there's
+        //  anything to read there).
         virtual void speculative_read (int handle_) = 0;
+
+        //  Start polling for availability of the socket for writing.
         virtual void set_pollout (int handle_) = 0;
+
+        //  Stop polling for availability of the socket for writing.
         virtual void reset_pollout (int handle_) = 0;
+
+        //  Try to write to the socket (even though we are not sure the
+        //  write will succeed).
         virtual void speculative_write (int handle_) = 0;
     };
 
