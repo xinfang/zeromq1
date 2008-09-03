cb30eea61ef8f2298f13be565549f99bc642d1fd
Martin Sustrik <sustrik@fastmq.com>
2008-09-03 10:13:01 +0000

    remote_lat.cpp fixed
    git-svn-id: http://svn.fastmq.org/svn/zeromq/trunk@418 7451a71c-b6c8-49cc-8254-141fc5361860

--- perf/tests/openamq/remote_lat.cpp
+++ perf/tests/openamq/remote_lat.cpp
@@ -1,12 +1,48 @@
+/*
+    Copyright (c) 2007-2008 FastMQ Inc.
+
+    This file is part of 0MQ.
+
+    0MQ is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    0MQ is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "../../transports/openamq.hpp"
-#include "../../workers/echo.hpp"
+#include "../scenarios/lat.hpp"
+
+using namespace std;
 
 int main (int argc, char *argv [])
 {
-    assert (argc == 2);
-    perf::openamq_t transport (argv [1]);
-    perf::echo_t worker (100000);
-    worker.run (transport);
+    if (argc != 4) {
+        cerr << "Usage: remote <hostname> <message size> <roundtrip count>"
+            << endl;
+        return 1;
+    }
+
+    //  Parse & print command line arguments.
+    const char *host = argv [1];
+    size_t msg_size = atoi (argv [2]);
+    int roundtrip_count = atoi (argv [3]);
+
+    cout << "message size: " << msg_size << " [B]" << endl;
+    cout << "roundtrip count: " << roundtrip_count << endl << endl;
+
+    //  Create OpenAMQ transport.
+    perf::zmq_t transport (host);
+
+    //  Do the job, for more detailed info refer to ../scenarios/lat.hpp.
+    perf::remote_lat (&transport, msg_size, roundtrip_count); 
+
     return 0;
 }
