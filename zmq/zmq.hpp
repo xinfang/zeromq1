a087163cff52bda7b535669a3c4326993ab1ba16
Pavol Malosek <malosek@fastmq.com>
2008-09-19 11:29:25 +0000

    added zmq.hpp main header file
    git-svn-id: http://svn.fastmq.org/svn/zeromq/trunk@449 7451a71c-b6c8-49cc-8254-141fc5361860

--- zmq/zmq.hpp
+++ zmq/zmq.hpp
@@ -17,29 +17,14 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <stddef.h>
+#ifndef __ZMQ_HPP_INCLUDED__
+#define __ZMQ_HPP_INCLUDED__
 
-#ifdef __cplusplus
-extern "C" {
+#include <zmq/locator.hpp>
+#include <zmq/dispatcher.hpp>
+#include <zmq/api_thread.hpp>
+#include <zmq/poll_thread.hpp>
+#include <zmq/message.hpp>
+
 #endif
 
-#define CZMQ_SCOPE_LOCAL 0
-#define CZMQ_SCOPE_GLOBAL 1
-
-typedef void (czmq_free_fn) (void *data_);
-
-void *czmq_create (const char *host_);
-void czmq_destroy (void *obj_);
-int czmq_create_exchange (void *obj_, const char *exchange_, int scope_,
-    const char *nic_);
-int czmq_create_queue (void *obj_, const char *queue_, int scope_,
-    const char *nic_);
-void czmq_bind (void *obj_, const char *exchange_, const char *queue_);
-void czmq_send (void *obj_, int eid_, void *data_, size_t size,
-    czmq_free_fn *ffn_);
-void czmq_receive (void *obj_, void **data_, size_t *size_,
-    czmq_free_fn **ffn_);
-
-#ifdef __cplusplus
-}
-#endif
