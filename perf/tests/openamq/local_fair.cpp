ab955ddaea18f1846630cac33058cd34f7524718
Martin Sustrik <sustrik@fastmq.com>
2008-09-24 14:25:35 +0000

    openamq fairness test
    git-svn-id: http://svn.fastmq.org/svn/zeromq/trunk@456 7451a71c-b6c8-49cc-8254-141fc5361860

---
+++ perf/tests/openamq/local_fair.cpp
@@ -0,0 +1,61 @@
+/*
+    Copyright (c) 2007-2008 FastMQ Inc.
+
+    This file is part of 0MQ.
+
+    0MQ is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    0MQ is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <unistd.h>
+#include <iostream>
+
+using namespace std;
+
+#include "../../transports/openamq.hpp"
+
+int main (int argc, char *argv [])
+{
+    if (argc != 6) {
+        cerr << "Usage: local_fair <hostname> <direct 0/1> <message size> "
+            "<message count> <number of consumers>" << endl;
+        return 1;
+    }
+
+    //  Parse & print command line arguments.
+    const char *host = argv [1];
+    bool direct = atoi (argv [2]);
+    size_t message_size = atoi (argv [3]);
+    int message_count = atoi (argv [4]);
+    int consumer_count = atoi (argv [5]);
+
+    cout << "direct: " << direct << endl;
+    cout << "message size: " << message_size << " [B]" << endl;
+    cout << "message count: " << message_count << endl;
+    cout << "number of consumers: " << consumer_count << endl;
+
+    //  Create connection to OpenAMQ broker.
+    perf::openamq_t transport (host, "cmds", "msgs", direct);
+
+    //  Wait till all the consumers are alive and running.
+    for (int consumer_nbr = 0; consumer_nbr < consumer_count; consumer_nbr++)
+        transport.receive ();
+
+    //  Send all the messages as fast as possible.
+    for (int message_nbr = 0; message_nbr != message_count; message_nbr ++)
+        transport.send (message_size);
+
+    sleep (100);
+
+    return 0;
+}
---
+++ perf/tests/openamq/remote_fair.cpp
@@ -0,0 +1,71 @@
+/*
+    Copyright (c) 2007-2008 FastMQ Inc.
+
+    This file is part of 0MQ.
+
+    0MQ is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    0MQ is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <assert.h>
+#include <iostream>
+
+using namespace std;
+
+#include "../../helpers/time.hpp"
+#include "../../transports/openamq.hpp"
+
+int main (int argc, char *argv [])
+{
+    if (argc != 5) {
+        cerr << "Usage: local_fair <hostname> <direct 0|1> <message size> "
+            "<message count>" << endl;
+        return 1;
+    }
+
+    //  Parse & print command line arguments.
+    const char *host = argv [1];
+    bool direct = atoi (argv [2]);
+    size_t message_size = atoi (argv [3]);
+    int message_count = atoi (argv [4]);
+
+    cout << "direct: " << direct << endl;
+    cout << "message size: " << message_size << " [B]" << endl;
+    cout << "message count: " << message_count << endl;
+
+    //  Create connection to OpenAMQ broker.
+    perf::openamq_t transport (host, "msgs", "cmds", direct);
+
+    //  Send notification to local that this instance of remote is running.
+    transport.send (1);
+
+    //  Receive all the remaining messages. Compute longest interval between
+    //  two subsequent messages.
+    uint64_t max_interval = 0;
+    perf::time_instant_t last_time = 0;
+    for (int message_nbr = 0; message_nbr != message_count; message_nbr ++) {
+        size_t size = transport.receive ();
+        assert (size == message_size);
+        perf::time_instant_t current_time = perf::now ();
+        uint64_t current_interval = current_time - last_time;
+        if (last_time &&  current_interval > max_interval)
+            max_interval = current_interval;
+        last_time = current_time;
+	printf ("%d\n", message_nbr);
+    }
+
+    cout << "Maximal interval between two subsequent messages: "
+        << (max_interval / 1000) << " [us]" << endl;
+
+    return 0;
+}
