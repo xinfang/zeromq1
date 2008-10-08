fe0098e2140deb8becbb64f049072496c7b25559
Martin Sustrik <sustrik@fastmq.com>
2008-10-08 13:11:06 +0000

    GPL licensing changed to LGPL, examples are still GPL'd
    git-svn-id: http://svn.fastmq.org/svn/zeromq/trunk@518 7451a71c-b6c8-49cc-8254-141fc5361860

--- zmq/bp_engine.hpp
+++ zmq/bp_engine.hpp
@@ -3,19 +3,20 @@
 
     This file is part of 0MQ.
 
-    0MQ is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
+    0MQ is free software; you can redistribute it and/or modify it under
+    the terms of the Lesser GNU General Public License as published by
     the Free Software Foundation; either version 3 of the License, or
     (at your option) any later version.
 
     0MQ is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    Lesser GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
+    You should have received a copy of the Lesser GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 
 #ifndef __ZMQ_BP_ENGINE_HPP_INCLUDED__
 #define __ZMQ_BP_ENGINE_HPP_INCLUDED__
