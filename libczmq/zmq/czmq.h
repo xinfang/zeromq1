cdd0d33846be378d053360c4d93d4ca0ea1d19a5
Tamara Kustarova <kustarova@fastmq.com>
2008-11-28 17:06:02 +0000

    changed the order of creating dispatcher and locator, because WSAStartUp in windows needs to be called before the locator is created
    git-svn-id: http://svn.fastmq.org/svn/zeromq/trunk@700 7451a71c-b6c8-49cc-8254-141fc5361860

--- libczmq/zmq/czmq.h
+++ libczmq/zmq/czmq.h
@@ -28,16 +28,16 @@ extern "C" {
 
 typedef void (czmq_free_fn) (void *data_);
 
-void *czmq_create (const char *host_);
-void czmq_destroy (void *obj_);
-int czmq_create_exchange (void *obj_, const char *exchange_, int scope_,
+void __declspec(dllexport) *czmq_create (const char *host_);
+void __declspec(dllexport) czmq_destroy (void *obj_);
+int __declspec(dllexport) czmq_create_exchange (void *obj_, const char *exchange_, int scope_,
     const char *nic_);
-int czmq_create_queue (void *obj_, const char *queue_, int scope_,
+int __declspec(dllexport) czmq_create_queue (void *obj_, const char *queue_, int scope_,
     const char *nic_);
-void czmq_bind (void *obj_, const char *exchange_, const char *queue_);
-void czmq_send (void *obj_, int eid_, void *data_, size_t size,
+void __declspec(dllexport) czmq_bind (void *obj_, const char *exchange_, const char *queue_);
+void __declspec(dllexport) czmq_send (void *obj_, int eid_, void *data_, size_t size,
     czmq_free_fn *ffn_);
-void czmq_receive (void *obj_, void **data_, size_t *size_,
+void __declspec(dllexport) czmq_receive (void *obj_, void **data_, size_t *size_,
     czmq_free_fn **ffn_);
 
 #ifdef __cplusplus
