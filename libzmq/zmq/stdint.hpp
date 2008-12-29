a4b51c59a153c5706c967164ab92bc490bf71b2e
Martin Sustrik <sustrik@fastmq.com>
2008-12-29 11:08:52 +0000

    basic integer types corrected for MSVC compiler
    git-svn-id: http://svn.fastmq.org/svn/zeromq/trunk@823 7451a71c-b6c8-49cc-8254-141fc5361860

--- libzmq/zmq/stdint.hpp
+++ libzmq/zmq/stdint.hpp
@@ -29,10 +29,14 @@
 #ifdef ZMQ_HAVE_MINGW32
 #include <stdint.h>
 #else
-typedef __int8 uint8_t;
-typedef __int16 uint16_t;
-typedef __int32 uint32_t;
-typedef __int64 uint64_t;
+typedef __int8 int8_t;
+typedef __int16 int16_t;
+typedef __int32 int32_t;
+typedef __int64 int64_t;
+typedef unsigned __int8 uint8_t;
+typedef unsigned __int16 uint16_t;
+typedef unsigned __int32 uint32_t;
+typedef unsigned __int64 uint64_t;
 #endif
 
 #else
