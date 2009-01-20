7450448c6d3da060fdfe4c2377e2de908c091c80
Martin Hurton <hurtonm@gmail.com>
2009-01-20 23:06:34 +0000

    simple implementation of disk-based message store
    git-svn-id: http://svn.fastmq.org/svn/zeromq/branches/v0.3.3@923 7451a71c-b6c8-49cc-8254-141fc5361860

---
+++ zmq/msg_store.cpp
@@ -0,0 +1,166 @@
+/*
+    Copyright (c) 2007-2008 FastMQ Inc.
+
+    This file is part of 0MQ.
+
+    0MQ is free software; you can redistribute it and/or modify it under
+    the terms of the Lesser GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    0MQ is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Lesser GNU General Public License for more details.
+
+    You should have received a copy of the Lesser GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string>
+
+#include <zmq/msg_store.hpp>
+
+zmq::msg_store_t::msg_store_t (const char *filename_, size_t filesize_) :
+    filename (filename_),
+    filesize (filesize_),
+    file_pos (0),
+    write_pos (0),
+    read_pos (0),
+    n_msgs (0)
+{
+    //  Open the message store file.
+    fd = open (filename.c_str (), O_RDWR | O_CREAT, 0600);
+    errno_assert (fd != -1);
+
+    //  Enable more aggresive read-ahead optimization.
+    int rc = posix_fadvise (fd, 0, filesize, POSIX_FADV_SEQUENTIAL);
+    errno_assert (rc == 0);
+}
+
+zmq::msg_store_t::~msg_store_t ()
+{
+    int rc = close (fd);
+    errno_assert (rc == 0);
+
+    rc = unlink (filename.c_str ());
+    errno_assert (rc == 0);
+}
+
+bool zmq::msg_store_t::store (message_t &msg)
+{
+    size_t msg_size = msg.size ();
+
+    //  Check buffer space availability.
+    //  NOTE: We always keep one byte open.
+    if (buffer_space () <= sizeof msg_size + msg_size)
+        return false;
+
+    //  Write the message length.
+    copy_to_file (&msg_size, sizeof msg_size);
+
+    //  Write the message body.
+    copy_to_file (msg.data (), msg.size ());
+
+    //  Update the message counter.
+    n_msgs ++;
+
+    return true;
+}
+
+void zmq::msg_store_t::fetch (message_t &msg)
+{
+    //  There must be at least one message available.
+    assert (n_msgs > 0);
+
+    //  Retrieve the message size.
+    size_t msg_size;
+    copy_from_file (&msg_size, sizeof msg_size);
+
+    //  Build the message.
+    msg.rebuild (msg_size);
+    copy_from_file (msg.data (), msg.size ());
+
+    //  Update the message counter.
+    n_msgs --;
+}
+
+bool zmq::msg_store_t::empty ()
+{
+    return n_msgs == 0;
+}
+
+unsigned long zmq::msg_store_t::size ()
+{
+    return n_msgs;
+}
+
+void zmq::msg_store_t::copy_from_file (void *buf_, size_t count_)
+{
+    char *ptr = (char *) buf_;
+    size_t n_read, n_left = count_;
+
+    while (n_left > 0) {
+
+        if (read_pos != file_pos) {
+            off_t offset = lseek (fd, read_pos, SEEK_SET);
+            errno_assert (offset == read_pos);
+            file_pos = read_pos;
+        }
+
+        if (n_left < filesize - read_pos)
+            n_read = n_left;
+        else
+            n_read = filesize - read_pos;
+
+        ssize_t n = read (fd, ptr, n_read);
+        errno_assert (n > 0);
+
+        ptr += n;
+        file_pos += n;
+        read_pos = (read_pos + n) % filesize;
+
+        n_left -= n;
+    }
+}
+
+void zmq::msg_store_t::copy_to_file (const void *buf_, size_t count_)
+{
+    char *ptr = (char *) buf_;
+    size_t n_write, n_left = count_;
+
+    while (n_left > 0) {
+
+        if (write_pos != file_pos) {
+            off_t offset = lseek (fd, write_pos, SEEK_SET);
+            errno_assert (offset == write_pos);
+            file_pos = write_pos;
+        }
+
+        if (n_left < filesize - write_pos)
+            n_write = n_left;
+        else
+            n_write = filesize - write_pos;
+
+        ssize_t n = write (fd, ptr, n_write);
+        errno_assert (n > 0);
+
+        ptr += n;
+        file_pos += n;
+        write_pos = (write_pos + n) % filesize;
+
+        n_left -= n;
+    }
+}
+
+size_t zmq::msg_store_t::buffer_space ()
+{
+    if (write_pos < read_pos)
+        return read_pos - write_pos;
+
+    return filesize - (write_pos - read_pos);
+}
---
+++ zmq/msg_store.hpp
@@ -0,0 +1,93 @@
+/*
+    Copyright (c) 2007-2008 FastMQ Inc.
+
+    This file is part of 0MQ.
+
+    0MQ is free software; you can redistribute it and/or modify it under
+    the terms of the Lesser GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    0MQ is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Lesser GNU General Public License for more details.
+
+    You should have received a copy of the Lesser GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __ZMQ_MSG_STORE_HPP_INCLUDED__
+#define __ZMQ_MSG_STORE_HPP_INCLUDED__
+
+#include <sys/types.h>
+#include <zmq/message.hpp>
+
+namespace zmq
+{
+
+    //  This class implements a persistent message store. Messages are
+    //  retrieved from the store in the same order in which they were
+    //  originally added.
+
+    class msg_store_t
+    {
+    public:
+
+        //  Initializes new message store.
+        msg_store_t (const char *filename, size_t filesize);
+
+        ~msg_store_t ();
+
+        //  Stores the message into the message store. The function
+        //  returns false if the message store is full and true otherwise.
+        bool store (message_t &msg);
+
+        //  Fetches the oldest message from the message store. It is an error
+        //  call this function when the message store is empty.
+        void fetch (message_t &msg);
+
+        //  Returns true if the message store is empty and false otherwise.
+        bool empty ();
+
+        //  Returns the number of messages kept in the message store.
+        unsigned long size ();
+
+    private:
+
+        //  Copies data from the memory buffer to the message store's file.
+        //  Wrap around when reaching maximum file size.
+        void copy_from_file (void *buf, size_t count);
+
+        //  Copies data from the message store's file to the memory buffer.
+        //  Wrap around when reaching end-of-file.
+        void copy_to_file (const void *buf, size_t count);
+
+        //  Returns the buffer space available.
+        size_t buffer_space ();
+
+        //  File descriptor to the message store's backing file.
+        int fd;
+
+        //  Name of the message store's backing file.
+        std::string filename;
+
+        //  Maximum size of the backing file.
+        size_t filesize;
+
+        //  File offset associated with the fd file descriptor.
+        off_t file_pos;
+
+        //  File offset the next message will be stored at.
+        off_t write_pos;
+
+        //  File offset the next message will be read from.
+        off_t read_pos;
+
+        //  Current number of messages kept in the message store.
+        unsigned long n_msgs;
+    };
+
+}
+
+#endif
