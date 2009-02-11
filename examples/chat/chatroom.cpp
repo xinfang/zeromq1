e524eff525d7441c6ccb40fb2aa96de43a851149
Martin Sustrik <sustrik@fastmq.com>
2009-02-11 22:09:41 +0000

    chatroom example made compilable
    git-svn-id: http://svn.fastmq.org/svn/zeromq/branches/cm@1023 7451a71c-b6c8-49cc-8254-141fc5361860

--- examples/chat/chatroom.cpp
+++ examples/chat/chatroom.cpp
@@ -30,47 +30,43 @@ using namespace zmq;
 
 bool error_handler (const char*)
 {
-    //  We don't want chatroom to fail when clent disconnects
+    //  We don't want chatroom to fail when clent disconnects.
     return true;
 }
 
 int main (int argc, const char *argv [])
 {
-    //  Check the command line syntax
-    if (argc != 5) {
-        cerr << "usage: chatroom <hostname> <chatroom name> "
-            "<in interface> <out interface>" << endl;
+    //  Check the command line syntax.
+    if (argc != 3) {
+        cerr << "usage: chatroom <hostname> <chatroom name> " << endl;
         return 1;
     }
 
-    //  Retrieve command line arguments
+    //  Retrieve command line arguments.
     const char *host = argv [1];
     const char *chatroom_name = argv [2];
-    const char *in_interface = argv [3];
-    const char *out_interface = argv [4];
 
-    //  Initialise 0MQ infrastructure
+    //  Initialise 0MQ infrastructure.
     set_error_handler (error_handler);
     dispatcher_t dispatcher (2);
     locator_t locator (host);
 
-    //  Initialise the thread layout
+    //  Initialise the thread layout.
     i_thread *pt = io_thread_t::create (&dispatcher);
     api_thread_t *api = api_thread_t::create (&dispatcher, &locator);
 
-    //  Create a queue to receive messages sent to the chatroom
+    //  Create a queue to receive messages sent to the chatroom.
     char tmp [16];
     zmq_snprintf (tmp, 16, "Q_%s", chatroom_name);
-    api->create_queue (tmp, scope_global, in_interface, pt, 1, &pt);
+    api->create_queue (tmp, scope_global, pt, 1, &pt);
 
-    //  Create an exchange to send messages from the chatroom
+    //  Create an exchange to send messages from the chatroom.
     zmq_snprintf (tmp, 16, "E_%s", chatroom_name);
-    int eid = api->create_exchange (tmp, scope_global, out_interface,
-        pt, 1, &pt);
+    int eid = api->create_exchange (tmp, scope_global, pt, 1, &pt);
 
     while (true) {
 
-        //  Get a message
+        //  Get a message.
         message_t in_message;
         api->receive (&in_message);
 
@@ -82,14 +78,14 @@ int main (int argc, const char *argv [])
         zmq_snprintf (timebuf, 256, ctime (&current_time));
         timebuf [strlen (timebuf) - 1] = ' ';
 
-        //  Create and fill in the message
+        //  Create and fill in the message.
         message_t out_message (strlen (timebuf) + in_message.size ());
         char *data = (char*) out_message.data ();
         memcpy (data, timebuf, strlen (timebuf));
         data += strlen (timebuf);
         memcpy (data, in_message.data (), in_message.size ());
 
-        //  Send the message
+        //  Send the message.
         api->send (eid, out_message);
     }
 }
